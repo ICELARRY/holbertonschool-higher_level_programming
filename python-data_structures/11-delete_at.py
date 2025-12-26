de8e0ea6ec3d1b4e0789da0861b8db953f602add
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-12-26 20:02:45 +0000

    Add 8-11 Python Data Structures exercises with tests

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    """Delete the item at a specific position in a list."""
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    # Rebuild the list without the element at idx
+    return my_list[:idx] + my_list[idx + 1:]
