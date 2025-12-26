580073f93d2c738655f47a6c106eeec24e575076
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-12-26 19:54:30 +0000

    Add divisible_by_2 function and main test file

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """Return a list of True/False depending on whether elements are divisible by 2."""
+    result = []
+    for num in my_list:
+        if num % 2 == 0:
+            result.append(True)
+        else:
+            result.append(False)
+    return result
