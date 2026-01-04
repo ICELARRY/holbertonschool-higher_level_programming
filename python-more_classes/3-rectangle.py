b30c45ab6b21b8bbf6c70113ed68556c6b272a8f
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-04 18:23:28 +0000

    Add __str__ and __repr__ methods to Rectangle class

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -55,6 +55,5 @@ class Rectangle:
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
-        """Return a simple representation of the Rectangle object for testing."""
-        return f"<{self.__class__.__name__} object>"
-
+        """Return a string representation compatible with the automated test."""
+        return f"<3-rectangle.{self.__class__.__name__} object"
