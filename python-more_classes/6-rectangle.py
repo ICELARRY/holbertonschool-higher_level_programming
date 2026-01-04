f69db6645c7d79499bfd4a4b38000029ab58a2cd
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-04 19:05:29 +0000

    6. Rectangle class with number_of_instances and deletion message

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -60,9 +60,8 @@ class Rectangle:
 
     def __repr__(self):
         """Return a string to recreate a new instance."""
-        return (
-            "Rectangle({}, {})".format(self.width, self.height)
-        )
+        repr_str = "Rectangle({}, {})".format(self.width, self.height)
+        return repr_str
 
     def __del__(self):
         """Print a message when an instance is deleted and decrement counter."""
