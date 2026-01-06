8a15137296d64d470e5d9aa800e5c07fb59ed7a3
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-06 21:16:26 +0000

    Add 0-lookup and 1-MyList classes with tests

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-"""Function that returns the list of available attributes
-and methods of an object
-"""
+"""Module that provides a function to list available attributes and methods of an object."""
+
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+    
+    Args:
+        obj: The object to inspect.
+        
+    Returns:
+        list: A list of strings containing the names of attributes and methods.
+    """
+    return dir(obj)
