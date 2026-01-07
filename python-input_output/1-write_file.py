1a58540e1650f80214107008d1e63838a9624443
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-07 21:48:28 +0000

    Add write_file function and test script with my_first_file.txt

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,5 +6,7 @@ and returns the number of characters written.
 
 def write_file(filename="", text=""):
     """Writes a string to a file and returns the number of characters written"""
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename,
+              "w",
+              encoding="utf-8") as f:
         return f.write(text)
