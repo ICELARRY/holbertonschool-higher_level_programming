710a33eadffc234f0a2741c685a5fd1a34cb4b9f
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-07 21:47:14 +0000

    Add write_file function and test script with my_first_file.txt

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Function that writes a string to a UTF-8 text file and returns the number of characters"""
+"""Function that writes a string to a UTF-8 text file
+and returns the number of characters written.
+"""
 
 
 def write_file(filename="", text=""):
