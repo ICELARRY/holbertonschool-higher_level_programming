dd016693e259f7b28f2fedfb375ce760d6992ef6
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-07 17:55:01 +0000

    Inheritance tasks: 0-lookup, 1-my_list, 2-is_same_class, 3-is_kind_of_class, 4-inherits_from completed

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module containing a function to check if an object is an instance of a
+class that inherited (directly or indirectly) from the specified class.
+"""
+
+
+def inherits_from(obj, a_class):
+    """Return True if obj is an instance of a subclass of a_class.
+
+    Returns False if obj is exactly an instance of a_class itself.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        bool: True if obj inherits from a_class (but is not exactly a_class),
+              False otherwise.
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
