18f3df6ab31c5100251fc5319fbed465e1e0e63c
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-08 19:44:16 +0000

    Add save_to_json_file function

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Write an object to a text file using JSON representation."""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Save an object to a file using JSON representation."""
+    with open(
+        filename,
+        "w",
+        encoding="utf-8"
+    ) as file:
+        json.dump(my_obj, file)
