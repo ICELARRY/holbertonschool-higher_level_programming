e2c9027bec578e057eb3a79b5e3080e6a3b35b8d
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-09 20:47:07 +0000

    Add 0-privileges.sql to show all privileges for user_0d_1 and user_0d_2

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- List all genres with the number of shows linked
+SELECT g.name AS genre, COUNT(tg.show_id) AS number_of_shows
+FROM genres g
+JOIN tv_show_genres tg ON g.id = tg.genre_id
+GROUP BY g.name
+ORDER BY number_of_shows DESC;
