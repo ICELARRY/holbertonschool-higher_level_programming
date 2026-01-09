7887226ddc6f40fc3b5e08434b2b6d61175bfa5f
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-09 20:56:32 +0000

    Add 0-privileges.sql to show all privileges for user_0d_1 and user_0d_2

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- 16. List shows and genres
+-- Lists all shows and their genres, shows without genres should display NULL
+-- Output columns: title, name
+-- Sorted by title and genre name
+
+SELECT ts.title, tg.name
+FROM tv_shows ts
+LEFT JOIN tv_show_genres tsg ON ts.id = tsg.show_id
+LEFT JOIN tv_genres tg ON tsg.genre_id = tg.id
+ORDER BY ts.title ASC, tg.name ASC;
