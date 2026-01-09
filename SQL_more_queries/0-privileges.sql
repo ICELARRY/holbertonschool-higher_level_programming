ae2922dffbb4746e7ee9c75f4452d6d25c235d26
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-09 20:17:31 +0000

    Add 0-privileges.sql to show all privileges for user_0d_1 and user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,11 +1,2 @@
--- Create users if they don't exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- (Optional) Grant all privileges so SHOW GRANTS shows something
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- Show grants for each user
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
