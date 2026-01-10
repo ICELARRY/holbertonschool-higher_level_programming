1f0958f525ceb7910131afaad64459d3907acbd4
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-10 09:34:43 +0000

    Style fixes and module documentation for 4-cities_by_state

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,43 +1,30 @@
 #!/usr/bin/python3
-"""
-Lists all cities from the database hbtn_0e_4_usa
-sorted by cities.id in ascending order
-"""
+"""Lists all cities from the database hbtn_0e_4_usa."""
+
 import MySQLdb
 import sys
 
-def main():
-    if len(sys.argv) != 4:
-        return
 
-    user = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
+def list_cities(username, password, database):
+    """Connects to the database and prints all cities with their state."""
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=user,
+        user=username,
         passwd=password,
         db=database
     )
-
-    cursor = db.cursor()
-
-    query = """
-    SELECT cities.id, cities.name, states.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    ORDER BY cities.id ASC
-    """
-    cursor.execute(query)
-
-    for row in cursor.fetchall():
+    cur = db.cursor()
+    cur.execute(
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id"
+    )
+    for row in cur.fetchall():
         print(row)
-
-    cursor.close()
+    cur.close()
     db.close()
 
-if __name__ == "__main__":
-    main()
 
+if __name__ == "__main__":
+    list_cities(sys.argv[1], sys.argv[2], sys.argv[3])
