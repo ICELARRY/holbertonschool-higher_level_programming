c57004a1b6efeba32f44b42949251d137ba581e0
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-10 10:34:03 +0000

    List all cities of a state safely

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,41 +1,43 @@
 #!/usr/bin/python3
 """
-This script lists all cities of a given state from the database hbtn_0e_4_usa.
-Usage: ./5-filter_cities.py <mysql username> <mysql password>
-                                  <database name> <state name>
-Results are sorted by cities.id in ascending order.
-SQL injection safe.
+Lists all cities of a given state from the database `hbtn_0e_4_usa`.
+Usage: ./5-filter_cities.py <username> <password> <database> <state_name>
 """
 
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: ./5-filter_cities.py <username> <password> <database> <state>")
+        print("Usage: ./5-filter_cities.py <username> <password> "
+              "<database> <state_name>")
         sys.exit(1)
 
-    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    username, password, database, state_name = sys.argv[1:]
 
     try:
-        db = MySQLdb.connect(host="localhost", port=3306,
-                             user=username, passwd=password,
-                             db=database, charset="utf8")
+        db = MySQLdb.connect(host="localhost",
+                             user=username,
+                             passwd=password,
+                             db=database,
+                             charset="utf8")
         cursor = db.cursor()
-        query = """
-        SELECT cities.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-        """
+
+        query = (
+            "SELECT cities.name "
+            "FROM cities "
+            "JOIN states ON cities.state_id = states.id "
+            "WHERE states.name = %s "
+            "ORDER BY cities.id"
+        )
         cursor.execute(query, (state_name,))
-        results = cursor.fetchall()
-        print(", ".join([row[0] for row in results]))
+        cities = [row[0] for row in cursor.fetchall()]
+
+        print(", ".join(cities))
+
+        cursor.close()
+        db.close()
+
     except MySQLdb.Error as e:
         print(f"Error: {e}")
-    finally:
-        if cursor:
-            cursor.close()
-        if db:
-            db.close()
+        sys.exit(1)
