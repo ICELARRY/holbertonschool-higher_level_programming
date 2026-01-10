3c4d3e9c0347e0bd5d827af5c12156dbdf9e6497
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-10 11:36:25 +0000

    List all State objects containing 'a' (PEP8 clean)

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-Prints the State object with the name passed as argument
+Print the State object with the name passed as argument
 from the database hbtn_0e_6_usa.
-Usage: ./10-model_state_my_get.py <username> <password> <database> <state_name>
 """
 
 import sys
@@ -10,18 +9,25 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>"
-              .format(sys.argv[0]))
+        print(
+            "Usage: {} <username> <password> <database> <state_name>"
+            .format(sys.argv[0])
+        )
         sys.exit(1)
 
-    user, password, db_name, state_name = sys.argv[1], sys.argv[2], \
-                                          sys.argv[3], sys.argv[4]
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
 
     # Connect to MySQL
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, password, db_name),
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            user, password, db_name
+        ),
         pool_pre_ping=True
     )
 
