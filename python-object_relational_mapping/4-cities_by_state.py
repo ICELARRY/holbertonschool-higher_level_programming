8781ec4e5c5820a91451b89e75470edbeb87864e
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-10 09:28:59 +0000

    List all cities by state

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+def main():
+    if len(sys.argv) != 4:
+        return
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    query = """
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id ASC
+    """
+    cursor.execute(query)
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
