814197b2bee92dc9bed558d3c3d968795b59bc73
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-10 09:12:33 +0000

    Fix case-sensitive filter for states starting with N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,30 +1,39 @@
 #!/usr/bin/python3
-"""Lists all states with names starting with N from a database."""
+"""
+This script lists all states with a name starting with N (uppercase)
+from the database hbtn_0e_0_usa.
+Usage: ./1-filter_states.py <username> <password> <database_name>
+"""
 
-import sys
 import MySQLdb
+import sys
 
 
 def main():
-    db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
+    """ Main function to fetch states starting with N """
+    if len(sys.argv) != 4:
+        return
 
-    cur = db.cursor()
-    cur.execute(
-        "SELECT id, name FROM states "
-        "WHERE name LIKE 'N%' "
-        "ORDER BY id ASC"
-    )
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-    for row in cur.fetchall():
+    # Connect to MySQL
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=user, passwd=password, db=db_name)
+
+    cursor = db.cursor()
+
+    # Execute SQL query with case-sensitive filter
+    query = "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC"
+    cursor.execute(query)
+
+    # Fetch and print all results
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
 
-    cur.close()
+    cursor.close()
     db.close()
 
 
