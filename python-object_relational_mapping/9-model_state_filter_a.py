d52940e50d5a52b1d4796cc4881c39dba7d0d3e2
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-10 11:34:01 +0000

    List all State objects containing 'a' (PEP8 clean)

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Lists all State objects that contain the letter 'a'
-from the database hbtn_0e_6_usa using SQLAlchemy.
-Usage: ./9-model_state_filter_a.py <username> <password> <database>
+Prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa.
+Usage: ./10-model_state_my_get.py <username> <password> <database> <state_name>
 """
 
 import sys
@@ -11,11 +11,13 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>"
+              .format(sys.argv[0]))
         sys.exit(1)
 
-    user, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    user, password, db_name, state_name = sys.argv[1], sys.argv[2], \
+                                          sys.argv[3], sys.argv[4]
 
     # Connect to MySQL
     engine = create_engine(
@@ -27,10 +29,12 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query all states containing 'a', ordered by id
-    states = session.query(State).filter(State.name.like("%a%")).order_by(State.id).all()
+    # Query the state by name safely
+    state = session.query(State).filter(State.name == state_name).first()
 
-    for state in states:
-        print(f"{state.id}: {state.name}")
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
 
     session.close()
